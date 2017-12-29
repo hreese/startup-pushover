ce5fa863ea5633d056913df703b826776474e5d0
Heiko Reese <mail@heiko-reese.de>
2017-12-29 01:48:59 +0100

    Fixed build scripts.

--- .build-all.sh
+++ .build-all.sh
@@ -1,6 +1,7 @@
 BIN_FILE_NAME_PREFIX=$1
 PROJECT_DIR=$2
-BIN_FILE_NAME_PREFIX=$1PROJECT_DIR=$2PLATFORMS=linux/386 linux/amd64 linux/arm linux/arm64 linux/mips linux/mips64 linux/mips64le linux/mipsle linux/ppc64 linux/ppc64le
+PLATFORMS="linux/386 linux/amd64 linux/arm linux/arm64 linux/mips linux/mips64 linux/mips64le linux/mipsle linux/ppc64 linux/ppc64le"
+
 for PLATFORM in $PLATFORMS; do
         GOOS=${PLATFORM%/*}
         GOARCH=${PLATFORM#*/}
@@ -10,7 +11,7 @@ for PLATFORM in $PLATFORMS; do
         BIN_FILE_NAME="$FILEPATH/${BIN_FILE_NAME_PREFIX}"
         #echo $BIN_FILE_NAME
         if [[ "${GOOS}" == "windows" ]]; then BIN_FILE_NAME="${BIN_FILE_NAME}.exe"; fi
-        CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -ldflags="-s -w" -o ${BIN_FILE_NAME}"
+        CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -ldflags=\"-s -w\" -o ${BIN_FILE_NAME}"
         #echo $CMD
         echo "${CMD}"
         eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
